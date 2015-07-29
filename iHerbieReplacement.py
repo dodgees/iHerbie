cfaa667cabe58785623be1ad38b3f0c03dc3a946
dodgees <dodgees2@gmail.com>
2015-07-29 13:18:11 -0500

    Multiple twitter handles
    Added OWHbigred to list of twitter handles. First successful link post,
    and first successful run selecting from multiple twitter accounts. Minor
    bug fixes associated with these changes.

--- iHerbieReplacement.py
+++ iHerbieReplacement.py
@@ -38,10 +38,10 @@ def getTwitterAPI():
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
-def getStatusScreenName(s):
+def getStatusScreenName(status):
     '''Takes a tweepy status object and returns the screen name associated with that object'''
-    status = s
-    json_str = json.dumps(status._json)
+    s = status
+    json_str = json.dumps(s._json)
     parsed_json = json.loads(json_str)
     return parsed_json['user']['screen_name'].encode('utf-8')
 
@@ -56,7 +56,11 @@ def insertTweets(tweetList, connection):
 
     #updated approach to setting tweetVals
     for t in tweets_list:
-        tweetVals = [str(t.id_str), getStatusScreenName(t), t.text.encode('utf-8'), str("https://twitter.com/statuses/" + t.id_str), int(t.favorite_count), int(t.retweet_count), str(t.created_at), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")]
+        tweetVals = [str(t.id_str), getStatusScreenName(t), repr(t.text.encode('utf-8')), str("https://twitter.com/statuses/" + t.id_str), int(t.favorite_count), int(t.retweet_count), str(t.created_at), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")]
+        for j in tweetVals:
+            print("%s" % str(j))
+        print()
+        print()
         curs.execute(query, tweetVals)
 
     conn.commit()
@@ -66,7 +70,7 @@ def main():
 
     api = getTwitterAPI()
 
-    tweets_list = get_tweets(['huskerextra'], api, tweets_list)
+    tweets_list = get_tweets(['huskerextra', 'OWHbigred'], api, tweets_list)
 
     tweets_list = sorted(tweets_list, key = getKey, reverse=True)
 
@@ -79,7 +83,7 @@ def main():
     search_query = '''SELECT COUNT(*) Post_Count FROM posted_tweets
                             where id = ? '''
 
-    postedVals = [str(tweets_list[0].id_str), getStatusScreenName(tweets_list[0]), tweets_list[0].text.encode('utf-8'), str('https://twitter.com/statuses/' + tweets_list[0].id_str), int(tweets_list[0].favorite_count), int(tweets_list[0].retweet_count), str(tweets_list[0].created_at), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")]
+    postedVals = [str(tweets_list[0].id_str), getStatusScreenName(tweets_list[0]), repr(tweets_list[0].text.encode('utf-8')), str('https://twitter.com/statuses/' + tweets_list[0].id_str), int(tweets_list[0].favorite_count), int(tweets_list[0].retweet_count), str(tweets_list[0].created_at), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")]
     curs = conn.cursor()
     curs.execute(search_query, [postedVals[0]])
 
@@ -89,6 +93,9 @@ def main():
     #This section needs refractored.
     post_count = (curs.fetchone())[0]
     if post_count == 0:
+        Config = ConfigParser.ConfigParser()
+        Config.read("configFile.ini")
+
         r = praw.Reddit(user_agent='iHerbie script')
         r.login(Config.get('Reddit', 'Username'), Config.get('Reddit', 'Password'))
         tweet_text = str(tweets_list[0].text.encode('utf-8'))
