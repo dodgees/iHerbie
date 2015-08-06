e1823623a506f760540f4601c4e79116ec705744
dodgees <dodgees2@gmail.com>
2015-08-06 09:53:12 -0500

    Adding testing conditions

--- iHerbie_test.py
+++ iHerbie_test.py
@@ -5,7 +5,12 @@ class TestHerbieMethods(unittest.TestCase):
     def test_cleanStatus_Text(self):
         status = "RT @ProFootballHOF: The order of #PFHOF15 enshrinement is ... http://t.co/bsT6CXYVHW"
         self.assertEqual(cleanStatusText(status), "The order of PFHOF15 enshrinement is ...")
-
+        status2 = "How cool is that! Last guy didn't even want the #Blackshirts ,this ones down to #ThrowTheBones !  #Huskers #GBR"
+        self.assertEqual(cleanStatusText(status2), "How cool is that! Last guy didn't even want the Blackshirts ,this ones down to ThrowTheBones !  Huskers GBR")
+        status3 = "@Huskers got a great one in @PhilBeckner ... A huge part of my success and progress as a man. He's a handful but he's  worth it lol"
+        self.assertEqual(cleanStatusText(status3), "Huskers got a great one in PhilBeckner ... A huge part of my success and progress as a man. He's a handful but he's  worth it lol")
+        status4 = "Welcome our new Assistant Coach @PhilBeckner  http://go.unl.edu/beckner080115 ."
+        self.assertEqual(cleanStatusText(status4), "Welcome our new Assistant Coach PhilBeckner   .")
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestHerbieMethods)
 unittest.TextTestRunner(verbosity=2).run(suite)
